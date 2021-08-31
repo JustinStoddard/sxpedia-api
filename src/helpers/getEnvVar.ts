ce952c8ff3f645eb252b482c8dbe255fd12b544b
Justin Stoddard <justinstoddard@Justins-MacBook-Pro.local>
2021-08-31 11:13:23 -0600

    added a comment for the helper

--- src/helpers/getEnvVar.ts
+++ src/helpers/getEnvVar.ts
@@ -1,4 +1,8 @@
-
+/**
+ * Function that will grab a value from process.env based on a key
+ * @param key Key of the value you want to grab
+ * @returns The value from process.env
+ */
 export const getEnvVar = (key: string) => {
   let value;
   try {
